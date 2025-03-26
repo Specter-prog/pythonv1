2fd7bab9feee56f606d41a889b1ea868e900ce91
Specter-prog <maria@Marias-MacBook-Pro.local>
2025-03-26 19:48:29 +0600

    added

--- base/project2.py
+++ base/project2.py
@@ -3,19 +3,20 @@ import requests
 import json
 from time import sleep
 
-url = 'https://www.kivano.kg/mobilnye-telefony'
-response = requests.get(url)
-html = response.text
-soup = bs(html, 'lxml')
-phones = soup.find_all('div', class_='item product_listbox oh')
+while True:
+    url = 'https://www.kivano.kg/mobilnye-telefony'
+    response = requests.get(url)
+    html = response.text
+    soup = bs(html, 'lxml')
+    phones = soup.find_all('div', class_='item product_listbox oh')
 
-phone_data = []
-for phone in phones:
-    name = phone.find('img').get('alt')
-    price = phone.find('div', class_='listbox_price').text.strip()
-    image = phone.find('img').get('src')
-    phone_data.append({'Model Name': name, 'Price': price, 'Image': image})
+    phone_data = []
+    for phone in phones:
+        name = phone.find('img').get('alt')
+        price = phone.find('div', class_='listbox_price').text.strip()
+        image = phone.find('img').get('src')
+        phone_data.append({'Model Name': name, 'Price': price, 'Image': image})
 
-with open('phones.json', 'w') as jsonfile:
-    json.dump(phone_data, jsonfile, indent = 4, ensure_ascii = False)
-sleep(3600)
+    with open('phones.json', 'w') as jsonfile:
+        json.dump(phone_data, jsonfile, indent = 4, ensure_ascii = False)
+    sleep(3600)
