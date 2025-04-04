3b319d6109edda64111c9be7a38636f8db9efa9f
Specter-prog <maria@Marias-MacBook-Pro.local>
2025-04-04 19:05:21 +0600

    added

--- oop/concepts.py
+++ oop/concepts.py
@@ -1,96 +1,123 @@
-'=================== Принципы ООП ================='
+'================Принципы ООП=============='
 # Наследование
 # Инкапсуляция
 # Полиморфизм
 # Абстракция
-# Ассоциация (Агрегация, Комопозиция)
+# Ассоциация (Агрегация, Композиция)
 
 
-'================== Наследование =================='
+'==============Наследование==============='
 # Наследование - принцип ООП, в котором мы можем унаследовать, переопределить и использовать в дочернем классе все аттрибуты и методы родительского класса
 
 class A:
+    var = 'a'
     def method(self):
         print('метод в классе А')
-
+    
 obj_a = A()
 obj_a.method() # метод в классе А
 
 class B(A):
-    pass
+    var = 'b'
 
 obj_b = B()
 obj_b.method()
 
-
 class C(A):
+    var = 'c'
     def method(self):
         print('метод в классе С')
-    
+
 obj_c = C()
 obj_c.method()
+print(obj_c.var)
+
 
 
 class Animal:
     price = 100000
 
     def voice(self):
-        ...
+        return 'Звуки животных'
+
 
 class Dog(Animal):
     def voice(self):
-        return 'bark'
+        return 'Гав-гав'
 
 class Cat(Animal):
-    def vocie(self):
-        return 'meow'
+    def voice(self):
+        return 'Мяу'
 
 class Duck(Animal):
     def voice(self):
-        return 'krya'
+        return 'Кря-кря'
+
 
 Bobik = Dog()
 Barsik = Cat()
 Donald = Duck()
 
-print(f'Bobik price: {Bobik.price} som, it can {Bobik.voice()}')
-print(f'Barsik price: {Barsik.price} som, it can {Barsik.voice()}')
-print(f'Donald price: {Donald.price} som, it can {Donald.voice()}')
+print(f'Бобик стоит - {Bobik.price} сом, он умеет {Bobik.voice()}')
+
+print(f'Барсик стоит - {Barsik.price} сом, он умеет {Barsik.voice()}')
+
+print(f'Дональд стоит - {Donald.price} сом, он умеет {Donald.voice()}')
+
+
+Marusa = Cat()
+print(Marusa.price)
 
 
 # одиночное наследование (1 род - 1 дочь)
-# иерархическое наследование (1 ряд - много дочь)
 class A:
     ...
-class B():
+
+class B(A):
     ...
 
-'----------------------------'
+'------------------------------'
+#иерархическое наследование (1 род - много дочь)
+class A:
+    ...
+
 class B(A):
     ...
 
 class C(A):
     ...
+
 class D(A):
     ...
-'''''''''''''''''''''''''''''''''' множестенное раследловюияя ;игшлря о 1 рлчь'''
 
+'---------------------------------'
+# множественное наследование (много род - 1 дочь)
 class A:
     ...
+
+class B:
+    ...
+
 class C(A, B):
     ...
 
-'--------------------------------'
-# многоуровненвое наследование 
-
-class А:
+'-----------------------------'
+# многоуровневое наследование - (цепочкой, род->доч/род->доч/род->доч)
+class A:
     ...
+
+class B(A):
+    ...
+
 class C(B):
     ...
+
 class D(C):
     ...
-'----------------------'
-# гибридное наследование - (смесь видов наследований)
+
+
+'----------------------------'
+# гибридное наследование - (смесь видов наследования)
 class A:
     ...
 class B(A):
@@ -102,3 +129,21 @@ class E:
 class D(C, E):
     ...
 
+
+
+class A:
+    attr = 12
+
+    
+class B(A):
+    def func(self):
+        return super().attr ** 2
+    
+
+obj = B()
+print(obj.func())
+
+
+__dict__ 
+
+object
